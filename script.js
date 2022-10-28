dcd9ed37c83c254a361260a4f4e017e03e2174f1
Daniel Barton <mineminecraftpe687@gmail.com>
2022-10-28 19:40:31 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var pops = 0;
+function playPop(popnumber) {
+    if (popnumber === 1){
+        setTimeout(() => { popcat.src = "src/close.png"; }, 100);
+        var audio = new Audio('src/sound/pop1.mp3');
+        audio.play();
+    }else{
+        setTimeout(() => { popcat.src = "src/close.png"; }, 100);
+        var audio = new Audio('src/sound/pop2.mp3');
+        audio.play();
+    }
+}
+
+function popcatgopop() {
+    pops+=1;
+    document.getElementById("count").innerHTML = pops;
+  var popcat = document.getElementById("popcat");
+  popcat.src = "src/open.png";
+  playPop(1);
+
+}
+
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 32) {
+        popcatgopop();
+    }
+});
