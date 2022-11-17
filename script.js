1e050f044d67c754d97876dc076199ba8a34b893
Daniel Barton <daniel4scratchhelp@gmail.com>
2022-11-18 09:18:00 +1100

    commit

--- script.js
+++ script.js
@@ -1,38 +1,32 @@
-var pops = 0;
-function playPop(popnumber) {
-    if (popnumber === 1){
-        setTimeout(() => { popcat.src = "src/close.png"; }, 100);
-        var audio = new Audio('src/sound/pop1.mp3');
-        audio.play();
-    }else{
-        setTimeout(() => { popcat.src = "src/close.png"; }, 100);
-        var audio = new Audio('src/sound/pop2.mp3');
-        audio.play();
-    }
+var pops = 0; //set clicks to 0
+function playPop() {//plays pop sound
+    setTimeout(() => { popcat.src = "src/close.png"; }, 100);
+    var audio = new Audio('src/sound/pop1.mp3');
+    audio.play();
 }
 
-function popcatgopop() {
-    pops+=1;
+function popcatgopop() {//pops the cat
+    pops += 1;
     document.getElementById("count").innerHTML = pops;
-  var popcat = document.getElementById("popcat");
-  popcat.src = "src/open.png";
-  playPop(1);
+    var popcat = document.getElementById("popcat");
+    popcat.src = "src/open.png";
+    playPop(); //play sound
 
 }
 
-document.addEventListener('keydown', function(event) {
-    if(event.keyCode == 32) {
+document.addEventListener('keydown', function (event) {//listens for space presses
+    if (event.keyCode == 32) {//space key
         popcatgopop();
-    } 
+    }
 });
 
-document.onclick= function(event){
+document.onclick = function (event) {
     popcatgopop();
 
 }
 
-window.onload = function() {
-    document.body.onselectstart = function() {
+window.onload = function () {
+    document.body.onselectstart = function () {
         return false;
     }
 }
