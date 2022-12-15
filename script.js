880e088bf8003294c3558575a92506e718e6ff1d
Daniel Barton <mineminecraftpe687@gmail.com>
2022-12-15 21:16:18 +1100

    (Test) Seeing if cookies work

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
-var pops = 0; //set clicks to 0
+console.log("Hello fellow computer nerd! So you want to cheat huh? Well I'll tell you how to cheat")
+console.log("Type in the following code:")
+console.log("var pops = 100000; //set pops to 100000")
+
+var popcatOpen = "src/open.png"
+var popcatClose = "src/close.png"
+
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+if (getCookie("pops") == null) {
+    setCookie("pops", 0, 365);
+    pops = 0;
+}
+else {
+    pops = getCookie("pops");
+    document.getElementById("count").innerHTML = pops;
+}
+
 function playPop() {//plays pop sound
-    setTimeout(() => { popcat.src = "src/close.png"; }, 100);
+    setTimeout(() => { popcat.src = popcatClose; }, 100);
     var audio = new Audio('src/sound/pop1.mp3');
     audio.play();
 }
 
 function popcatgopop() {//pops the cat
     pops += 1;
+    setCookie("pops", pops, 365);
     document.getElementById("count").innerHTML = pops;
     var popcat = document.getElementById("popcat");
-    popcat.src = "src/open.png";
+    popcat.src = popcatOpen;
     playPop(); //play sound
 
 }
