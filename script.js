60c33ed73471a4e0c64fded69faefa363da01a8c
Daniel Barton <mineminecraftpe687@gmail.com>
2022-12-20 08:28:12 +1100

    Major bug fixes... again 🙄

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
 var popcatOpen = "src/open.png"
 var popcatClose = "src/close.png"
 
-let pops = 0
-const storedClicks = localStorage.getItem('clicks');
-pops = storedClicks;
 
-function playPop() {//plays pop sound
+let pops = 0;
+if (localStorage.getItem("clicks") === null | localStorage.getItem("clicks") === NaN | localStorage.getItem("clicks") === undefined | localStorage.getItem("clicks") === "NaN") {
+    localStorage.setItem('clicks', pops);
+  } else {
+    const storedClicks = localStorage.getItem('clicks');
+   pops = storedClicks;
+  }
+
+
+//play sound
+function playPop() {
     setTimeout(() => { popcat.src = popcatClose; }, 100);
     var audio = new Audio('src/sound/pop1.mp3');
     audio.play();
 }
-
-function popcatgopop() {//pops the cat
+//popcatgopop function
+function popcatgopop() {
     pops = parseInt(pops);
     pops += 1;
     localStorage.setItem('clicks', pops);
     document.getElementById("count").innerHTML = pops;
     const popcat = document.getElementById("popcat");
     popcat.src = popcatOpen;
-    playPop(); //play sound
+    playPop(); 
 
 }
+
+
+
+function resetPops() {
+    localStorage.setItem('clicks', 0);
+    location.reload();
+}
+
+
+function setPops(input) {
+    var input = prompt("Set the pop count to:", input);
+    pops = input;
+    localStorage.setItem('clicks', pops);
+    document.getElementById("count").innerHTML = pops;
+}
+
+//listens for space presses and clicks
 
 document.addEventListener('keydown', function (event) {//listens for space presses
     if (event.keyCode == 32) {//space key
